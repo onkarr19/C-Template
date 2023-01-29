eced69c53fd3c07ae7f4e9d704dcfb004b9cb772
onkar <onkarchendage@gmail.com>
2023-01-29 18:03:19 +0530

    v1.1 typedef lowercase

--- template.cpp
+++ template.cpp
@@ -3,14 +3,15 @@
 #include<vector>
 using namespace std;
 
-typedef long long LL;
-typedef pair<int,int> PII;
-typedef pair<long,long> PLL;
-typedef pair<string, string> PSS;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<long,long> pll;
+typedef pair<string, string> pss;
 
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef vector<long> vl;
+typedef vector<pii> vii;
+typedef vector<ll> vl;
 typedef vector<vl> vll;
 typedef vector<string> vs;
 
