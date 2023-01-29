c17b3eb357a52073ba9b2fbd49852a49fe7e7815
onkar <onkarchendage@gmail.com>
2023-01-29 13:12:41 +0530

    v1.1 typedef add

--- template.cpp
+++ template.cpp
@@ -3,9 +3,24 @@
 #include<vector>
 using namespace std;
 
+typedef long long LL;
+typedef pair<int,int> PII;
+typedef pair<long,long> PLL;
+typedef pair<string, string> PSS;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<long> vl;
+typedef vector<vl> vll;
+typedef vector<string> vs;
+
+
+
 int main() {
 
     cout << "Working...";
+
+
 
 
     return 0;
