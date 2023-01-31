35d690146de2001238c75a599afc01a0d2bac02a
onkar <onkarchendage@gmail.com>
2023-01-31 10:33:51 +0530

    v1.2 for loop ops add

--- template.cpp
+++ template.cpp
@@ -15,6 +15,30 @@ typedef vector<ll> vl;
 typedef vector<vl> vll;
 typedef vector<string> vs;
 
+#define FOR(a,b,c) for (int (a)=(b);(a)<(c);++(a))
+#define FORN(a,b,c) for (int (a)=(b);(a)<=(c);++(a))
+#define FORD(a,b,c) for (int (a)=(b);(a)>=(c);--(a))
+#define FORSQ(a,b,c) for (int (a)=(b);(a)*(a)<=(c);++(a))
+#define FOREACH(a,b) for (auto &(a) : (b))
+#define REP(i,n) FOR(i,0,n)
+#define REPN(i,n) FORN(i,1,n)
+#define MAX(a,b) a = max(a,b)
+#define MIN(a,b) a = min(a,b)
+#define SQR(x) ((LL)(x) * (x))
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ALL(v) v.begin(),v.end()
+#define ALLA(arr,sz) arr,arr+sz
+#define SIZE(v) (int)v.size()
+#define SORT(v) sort(ALL(v))
+#define REVERSE(v) reverse(ALL(v))
+#define SORTA(arr,sz) sort(ALLA(arr,sz))
+#define REVERSEA(arr,sz) reverse(ALLA(arr,sz))
+
+#define TC(t) while(t--)
 
 
 int main() {
